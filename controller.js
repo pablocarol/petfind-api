232ef8b791d0507a5544dadc031188d8c5ebd09b
pablocarol <47698187+pablocarol@users.noreply.github.com>
2023-03-22 12:37:54 +0100

    2 endpoints working

--- controller.js
+++ controller.js
@@ -2,20 +2,19 @@ const db = require("./database.js");
 
 async function all(req, res) {
     try {
-      const rows = await db.all();
-      console.log(rows);
-      res.json(rows);
+        const rows = await db.all();
+        res.json(rows[0]);
     } catch (ex) {
-      res.status(500).json({ error: err });
+        res.status(500).json({});
     }
 }
 
-async function getLastLocation (req, res) {
+async function getLastLocation(req, res) {
     try {
         const rows = await db.getLastLocation();
-        res.json(rows);
+        res.json(rows[0]);
     } catch (ex) {
-        res.status(500).json({ error: err });
+        res.status(500).json({});
     }
 }
 
