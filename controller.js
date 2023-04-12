d0f972b33c54e91d1b89eef90159c57f00374cf5
Pablo Carol <pablo.carol@students.salle.url.edu>
2023-04-12 20:27:41 +0200

    feat: geojson

--- controller.js
+++ controller.js
@@ -1,24 +1,56 @@
 const db = require("./database.js");
 
 async function all(req, res) {
-    try {
-        const rows = await db.all();
-        res.json(rows[0]);
-    } catch (ex) {
-        res.status(500).json({});
-    }
+  try {
+    const rows = await db.all();
+    const features = rows[0].map((row) => {
+        return {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [row.lon, row.lat]
+          },
+          properties: {
+            // agregar cualquier propiedad que desees aquí
+          }
+        };
+      });
+      const geojsonData = {
+        type: 'FeatureCollection',
+        features: features
+      };
+      res.json(geojsonData);
+  } catch (ex) {
+    res.status(500).json({});
+  }
 }
 
 async function getLastLocation(req, res) {
-    try {
-        const rows = await db.getLastLocation();
-        res.json(rows[0]);
-    } catch (ex) {
-        res.status(500).json({});
-    }
+  try {
+    const rows = await db.getLastLocation();
+    const data = rows[0].pop();
+    const geojsonData = {
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: [data.lon, data.lat],
+          },
+          properties: {
+            // agregar cualquier propiedad que desees aquí
+          },
+        },
+      ],
+    };
+    res.json(geojsonData);
+  } catch (ex) {
+    res.status(500).json({});
+  }
 }
 
 module.exports = {
-    getLastLocation,
-    all
-}
+  getLastLocation,
+  all,
+};
